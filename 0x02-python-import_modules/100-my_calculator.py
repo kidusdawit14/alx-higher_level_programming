61a1e0ca925ba951e679148e16e266034a323ba7
Kidus Getachew <d.kidus@gmail.com>
2022-06-02 18:52:22 +0200

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
+    from calculator_1.py import add, sub, mul, div
     import sys
 
     num_args = len(sys.argv)
